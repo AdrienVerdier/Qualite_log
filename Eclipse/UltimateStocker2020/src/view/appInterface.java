2dfd6e8637d12c113a23827045d196a3ed3b712b
AdrienVerdier <adrien.verdier@gmail.com>
2020-01-09 10:16:00 +0100

    debut view
    début de la vue

---
+++ Eclipse/UltimateStocker2020/src/view/appInterface.java
@@ -0,0 +1,40 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+/**
+ * This class represents the frame of the Application
+ * @author Pierre Savary & Adrien Verdier
+ *
+ */
+public class appInterface extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	public static int windowsSizeX = 1000;
+	public static int windowsSizeY = 800;
+
+	/**
+	 * This method creates the frame
+	 */
+	public appInterface() {
+		
+		try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+		this.setTitle("Application de gestion de fichier");
+		this.setResizable(false);
+		this.setSize(windowsSizeX, windowsSizeY);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel authentificationPanel = new authentificationPanel(this);
+
+		this.setVisible(true);
+	}
+
+}
